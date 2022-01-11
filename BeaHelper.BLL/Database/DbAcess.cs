916357a3a90de89ab44dc5587e053067410cd337
Jonathan Scrok <jonathanwilliamscrok@gmail.com>
2022-01-10 22:39:49 -0300

    alteração connectionString

--- BeaHelper.BLL/Database/DbAcess.cs
+++ BeaHelper.BLL/Database/DbAcess.cs
@@ -13,8 +13,9 @@ namespace BeaHelper.BLL.Database
         public static string GetConnection()
         {
             //var connString = DbAcess.Configuration["ConnectionString"];
-            //var connString = "Data Source=mssql-62760-0.cloudclusters.net,12044;Initial Catalog=be_helper;Integrated Security=False;User Id=SuperBeaHelper;Password=B3ah3lper#2021;MultipleActiveResultSets=True";
-            var connString = "Data Source=.\\SQLEXPRESS; Initial Catalog=be_helper; User Id=sa; Password=b3ah3lper#2021;";
+
+            var connString = "Data Source=mssql-65758-0.cloudclusters.net,10403;Initial Catalog=be_helper;Integrated Security=False;User Id=SuperBeaHelper;Password=B3ah3lper#2021;MultipleActiveResultSets=True";
+            //var connString = "Data Source=.\\SQLEXPRESS; Initial Catalog=be_helper; User Id=sa; Password=b3ah3lper#2021;";
             return connString;
         }
 
