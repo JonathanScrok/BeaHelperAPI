750a0786a8b1db8d901a4aa751f7a5221639de6a
Jonathan Scrok <jonathanwilliamscrok@gmail.com>
2022-01-19 19:47:38 -0300

    Retornando usuario completo no login

--- BeaHelperAPI/Controllers/LoginController.cs
+++ BeaHelperAPI/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using BeaHelper.BLL.BD;
+﻿using AutoMapper;
+using BeaHelper.BLL.BD;
 using BeaHelper.BLL.Models;
 using BeaHelper.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +25,7 @@ namespace BeaHelperAPI.Controllers
         //return Ok(); Status 200 -  operação deu certo poderá passar um objeto como parametro
 
         private readonly ILogger<LoginController> _logger;
+
         public LoginController(ILogger<LoginController> logger)
         {
             _logger = logger;
@@ -76,12 +78,38 @@ namespace BeaHelperAPI.Controllers
             try
             {
                 string senhaEncoding = _encodeSenha.HashValue(senha);
-
                 var model = Login_P1.BuscaLogin_EmailSenha(email, senhaEncoding);
 
                 if (model.Count > 0)
                 {
-                    return Ok(model);
+
+                    Mapper.CreateMap<Usuario_P1, UsuarioCompleto>();
+
+                    Usuario_P1 usuario = new Usuario_P1(model[0].Email);
+                    usuario.CompleteObjectEmail();
+
+                    var usuarioCompleto = Mapper.Map<UsuarioCompleto>(usuario);
+
+                    var Avaliacao = Avaliacao_P1.TodasAvaliacoesUsuario(usuario.IdUsuario);
+
+                    if (Avaliacao.Count > 0)
+                    {
+                        double NotaSomadas = 0;
+                        for (int i = 0; i < Avaliacao.Count; i++)
+                        {
+                            NotaSomadas += Avaliacao[i].Nota;
+                        }
+                        var media = NotaSomadas / Avaliacao.Count;
+                        media = Math.Round(media, 1);
+                        usuarioCompleto.NotaMedia = media;
+                        usuarioCompleto.NuncaAvaliado = false;
+                    }
+                    else
+                    {
+                        usuarioCompleto.NuncaAvaliado = true;
+                    }
+
+                    return Ok(usuarioCompleto);
                 }
                 else
                 {
